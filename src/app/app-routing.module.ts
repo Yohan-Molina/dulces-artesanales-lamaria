24e9f68d217cf42313738eff4612f1640b142798
yohan molina <ymolina@existaya.com>
2022-09-18 10:54:49 -0500

    Se estructura vista principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', loadChildren: () => import('./pages/features/home/home.module').then(m => m.HomeModule) }, 
+  { path: '**', loadChildren: () => import('./pages/features/not-found/not-found.module').then(m => m.NotFoundModule) },
+];
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
